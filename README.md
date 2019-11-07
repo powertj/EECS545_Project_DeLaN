40ca34245abe544da1fc884ff78e132fdf7bf3c9
Thomas Jack Power <tpower@armeclipse.eecs.umich.edu>
2019-11-07 13:50:03 -0500

    add setup.py for install

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Project repo for EECS 545 Fall 2019:
 
 Deep Lagrangian Networks
+Requires python 3.5 +
+install via pip install -e .
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='delan',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Thomas Power, Craig Knuth, Matt Philippi, Kaustav Chakraborty',
+    author_email='tpower@umich.edu',
+    description='',
+)
