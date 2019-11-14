ca5768f313801083d90542c249d2dc25d6a13bf0
Craig Knuth <cknuth@umich.edu>
2019-11-14 13:25:12 -0500

    Added code to generate cartpole trajectories

---
+++ cartpole_traj_gen/add_casadi_mpctools.m
@@ -0,0 +1,7 @@
+% requires installzation of casadi and mpctools
+% Matlab R2015b or later
+% Casadi >= 3.0: http://files.casadi.org
+% MPCTools, must download and unzip:
+%   https://bitbucket.org/rawlings-group/octave-mpctools
+addpath('/home/craigknuth/MATLAB Add-Ons/Toolboxes/casadi')  % change to your installation path
+addpath('/home/craigknuth/MATLAB Add-Ons/Toolboxes/mpctools')
