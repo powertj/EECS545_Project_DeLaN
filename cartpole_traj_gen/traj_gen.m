886ef54b2ffe1adbc5b9bbe5fde268c62431639b
cknuth <cknuth@wisc.edu>
2019-11-30 15:25:13 -0500

    update cartpole trajectory data

--- cartpole_traj_gen/traj_gen.m
+++ cartpole_traj_gen/traj_gen.m
@@ -2,22 +2,29 @@ add_casadi_mpctools;
 
 mpc = import_mpctools();
 
+% cartpole params (should match what's in run_mpc_sim
+params.M = 10; % mass of cart
+params.m = 1; % mass of pendulum
+params.l = 1; % pendulum length
+params.g = 9.81; % gravity
+
 goal_x = 1:0.1:2;
+Nsim = length(goal_x);
 sim_length = 20*25;
-trajs = zeros(Nsim,sim_length,6);
-Hs = zeros(Nsim,sim_length,2,2);
-Gs = zeros(Nsim,sim_length,2);
-Us = zeros(Nsim,sim_length,2);
+trajectories = zeros(Nsim,sim_length,6);
+H = zeros(Nsim,sim_length,2,2);
+g = zeros(Nsim,sim_length,2);
+torques = zeros(Nsim,sim_length,2);
 for k=1:length(goal_x)
     data = run_mpc_sim([goal_x(k); pi; 0; 0], sim_length, mpc);
-    trajs(k,:,1:2) = data.x(1:2,1:end-1).';
+    trajectories(k,:,1:2) = data.x(1:2,1:end-1).';
     for i=1:sim_length
-        trajs(k,i,3:6) = cartpole_dynamics(data.x(:,i), data.u(:,i), params).';
-        Us(k,i,:) = [data.u(:,i) 0];
-        Gs(k,i,2) = params.m * params.g * params.l * sin(trajs(k,i,2));
-        Hs(k,i,:,:) = [params.m + params.M, params.m * params.l * cos(trajs(k,i,2)); ...
-            params.m * params.l * cos(trajs(k,i,2)), params.m * params.l^2];
+        trajectories(k,i,3:6) = cartpole_dynamics(data.x(:,i), data.u(:,i), params).';
+        torques(k,i,:) = [data.u(:,i) 0];
+        g(k,i,2) = params.m * params.g * params.l * sin(trajectories(k,i,2));
+        H(k,i,:,:) = [params.m + params.M, params.m * params.l * cos(trajectories(k,i,2)); ...
+            params.m * params.l * cos(trajectories(k,i,2)), params.m * params.l^2];
     end
 end
 
-save('cartpole_traj_out.mat', 'trajs', 'Us', 'Gs', 'Hs')
+save('cartpole_traj_out.mat', 'trajectories', 'torques', 'g', 'H')
