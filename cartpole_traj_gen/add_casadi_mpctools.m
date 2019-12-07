2fdc7de045850cd5cc8c9914d041c6675bd6d368
cknuth <cknuth@wisc.edu>
2019-12-07 10:20:24 -0500

    remove my name from file

--- cartpole_traj_gen/add_casadi_mpctools.m
+++ cartpole_traj_gen/add_casadi_mpctools.m
@@ -3,5 +3,5 @@
 % Casadi >= 3.0: http://files.casadi.org
 % MPCTools, must download and unzip:
 %   https://bitbucket.org/rawlings-group/octave-mpctools
-addpath('/home/craigknuth/MATLAB Add-Ons/Toolboxes/casadi')  % change to your installation path
-addpath('/home/craigknuth/MATLAB Add-Ons/Toolboxes/mpctools')
+addpath('\yourpathhere\matlab-add-ons\casadi-windows-matlabR2016a-v3.5.1')  % change to your installation path
+addpath('\yourpathhere\Documents\matlab-add-ons\mpctools\mpctools')
